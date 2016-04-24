8d9e0a4cfb0e8bae63414facf6bc9e7dbffa9fe0
Dylan Pinn <dylan.pinn@gmail.com>
2016-04-24 18:20:27 +1000

    Exercise 1.1-1.3

---
+++ 1.1-the-elements-of-programming/exercise-1.3.scm
@@ -0,0 +1,7 @@
+;; Exercise 1.3: Define a procedure that takes three numbers as arguments and returns the sum of the squares of the larger numbers.
+
+(define (square x) (* x x))
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+(define (ex x y z)
+  (cond ((> x y) (sum-of-squares x y))))
