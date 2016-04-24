890d0aef6bf2d87f928f177b6785c850ddd4f0af
Dylan Pinn <dylan.pinn@gmail.com>
2016-04-24 18:24:22 +1000

    Clean up comments

--- 1.1-the-elements-of-programming/exercise-1.3.scm
+++ 1.1-the-elements-of-programming/exercise-1.3.scm
@@ -1,4 +1,6 @@
-;; Exercise 1.3: Define a procedure that takes three numbers as arguments and returns the sum of the squares of the larger numbers.
+;; Exercise 1.3: Define a procedure that takes three numbers as
+;;               arguments and returns the sum of the squares of
+;;               the larger numbers.
 
 (define (square x) (* x x))
 (define (sum-of-squares x y)
